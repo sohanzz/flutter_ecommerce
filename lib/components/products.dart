cada58a572d81b4df145c7f85b62158fbc2b41e1
asif <asifsohanz@yahoo.com>
2020-02-17 22:14:18 +0600

    Similar products

--- lib/components/products.dart
+++ lib/components/products.dart
@@ -32,7 +32,7 @@ class _ProductsState extends State<Products> {
 
     {
       "name": "Pant",
-      "picture": "images/products/pants1.jpeg",
+      "picture": "images/products/pants2.jpeg",
       "old_price": "445",
       "price": "225",
     },
@@ -52,7 +52,7 @@ class _ProductsState extends State<Products> {
             new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
         itemBuilder: (BuildContext context, int index) {
           return Single_prod(
-            product_name: product_list[index]['name'],
+            prod_name: product_list[index]['name'],
             prod_picture: product_list[index]['picture'],
             prod_old_price: product_list[index]['old_price'],
             prod_price: product_list[index]['price'],
@@ -62,13 +62,13 @@ class _ProductsState extends State<Products> {
 }
 
 class Single_prod extends StatelessWidget {
-  final product_name;
+  final prod_name;
   final prod_picture;
   final prod_old_price;
   final prod_price;
 
   Single_prod({
-    this.product_name,
+    this.prod_name,
     this.prod_picture,
     this.prod_old_price,
     this.prod_price,
@@ -78,13 +78,13 @@ class Single_prod extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Hero(
-        tag: product_name,
+        tag: prod_name,
         child: Material(
           child: InkWell(
             onTap: () => Navigator.of(context).push(new MaterialPageRoute(
               //Here we re passing the values of the product to the product details page
                 builder: (context) => new ProductDetails(
-                  product_detail_name: product_name,
+                  product_detail_name: prod_name,
                   product_detail_new_price: prod_price,
                   product_detail_old_price: prod_old_price,
                   product_detail_picture: prod_picture,
@@ -92,25 +92,13 @@ class Single_prod extends StatelessWidget {
             child: GridTile(
               footer: Container(
                 color: Colors.white70,
-                child: ListTile(
-                  leading: Text(
-                    product_name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                child: Row (
+                  children: <Widget>[
+                    Expanded(
+                      child: new Text(prod_name, style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
                     ),
-                  ),
-                  title: Text(
-                    "\$$prod_price",
-                    style: TextStyle(
-                        color: Colors.redAccent, fontWeight: FontWeight.w800),
-                  ),
-                  subtitle: Text(
-                    "\$$prod_old_price",
-                    style: TextStyle(
-                        color: Colors.black54,
-                        fontWeight: FontWeight.w800,
-                        decoration: TextDecoration.lineThrough),
-                  ),
+                    Text("\$${prod_price}", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),)
+                  ],
                 ),
               ),
               child: Image.asset(
--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -6,13 +6,11 @@ class ProductDetails extends StatefulWidget {
   final product_detail_old_price;
   final product_detail_picture;
 
-
-  ProductDetails({
-    this.product_detail_name,
-    this.product_detail_new_price,
-    this.product_detail_old_price,
-    this.product_detail_picture
-  });
+  ProductDetails(
+      {this.product_detail_name,
+      this.product_detail_new_price,
+      this.product_detail_old_price,
+      this.product_detail_picture});
 
   @override
   _ProductDetailsState createState() => _ProductDetailsState();
@@ -22,26 +20,26 @@ class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-          elevation: 0.5,
-          backgroundColor: Colors.red,
-          centerTitle: false,
-          title: Text('Shop App'),
-          actions: <Widget>[
-            new IconButton(
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.white,
-                ),
-                onPressed: () {}),
-            new IconButton(
-                icon: Icon(
-                  Icons.shopping_cart,
-                  color: Colors.white,
-                ),
-                onPressed: () {})
-          ],
-        ),
+      appBar: new AppBar(
+        elevation: 0.5,
+        backgroundColor: Colors.red,
+        centerTitle: false,
+        title: Text('Shop App'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
+        ],
+      ),
       body: new ListView(
         children: <Widget>[
           new Container(
@@ -51,8 +49,243 @@ class _ProductDetailsState extends State<ProductDetails> {
                 color: Colors.white,
                 child: Image.asset(widget.product_detail_picture),
               ),
-            ) ,
-          )
+              footer: new Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: new Text(
+                    widget.product_detail_name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  title: new Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text(
+                          "\$${widget.product_detail_old_price}",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough),
+                        ),
+                      ),
+                      Expanded(
+                        child: new Text(
+                          "\$${widget.product_detail_new_price}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.red),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          //======== The first button ========//
+
+          Row(
+            children: <Widget>[
+              // ======= The size Button========//
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: Text("Size"),
+                            content: Text("Choose the size"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: Text("close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text("Size"),
+                      ),
+                      Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: Text("Colors"),
+                            content: Text("Choose the color"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: Text("close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text("Color"),
+                      ),
+                      Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return new AlertDialog(
+                            title: Text("Quantity"),
+                            content: Text("Choose the quantity"),
+                            actions: <Widget>[
+                              new MaterialButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop(context);
+                                },
+                                child: Text("close"),
+                              )
+                            ],
+                          );
+                        });
+                  },
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 0.2,
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: new Text("Qty"),
+                      ),
+                      Expanded(
+                        child: new Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          //======= The second button======//
+
+          Row(
+            children: <Widget>[
+              // ======= The size Button========//
+
+              Expanded(
+                child: MaterialButton(
+                    onPressed: () {},
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    elevation: 0.2,
+                    child: new Text("Buy now")),
+              ),
+
+              new IconButton(
+                  icon: Icon(
+                    Icons.add_shopping_cart,
+                    color: Colors.red,
+                  ),
+                  onPressed: () {}),
+              new IconButton(
+                  icon: Icon(
+                    Icons.favorite_border,
+                    color: Colors.red,
+                  ),
+                  onPressed: () {}),
+            ],
+          ),
+
+          Divider(),
+          new ListTile(
+            title: Text("Product details"),
+            subtitle: Text(
+                "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book."),
+          ),
+          Divider(),
+
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 5, 5, 5),
+                child: new Text(
+                  "Product name",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: new Text(widget.product_detail_name),
+              ),
+            ],
+          ),
+
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 5, 5, 5),
+                child: new Text(
+                  "Product Brand",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+
+              //==========PRODUCT BRAND=======//
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: new Text("Brand X"),
+              ),
+            ],
+          ),
+
+          // Add the condition
+          new Row(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 5, 5, 5),
+                child: new Text(
+                  "Product condition",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+                child: new Text("New"),
+              ),
+            ],
+          ),
         ],
       ),
     );
